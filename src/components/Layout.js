a87890fe347d2b527c6ccdfe2ab21568c220c634
Dejan85 <xypnotica@gmail.com>
2021-04-30 22:21:02 +0200

    added normilise css

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,8 @@
 import React from "react";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import "normalize.css";
+import "../assets/css/main.css";
 
 const Layout = props => {
   const { children } = props;
