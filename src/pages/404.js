995e31709b41f725e450a95c67157717caf1599c
Dejan85 <xypnotica@gmail.com>
2021-04-30 22:16:18 +0200

    added css file

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Layout from "../components/Layout";
 
 const Error = () => {
-  return <div>Error Page</div>;
+  return (
+    <Layout>
+      <div>
+        <h1>Error page</h1>
+      </div>
+    </Layout>
+  );
 };
 
 export default Error;
