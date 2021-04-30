7e3f6c0066843c4b3300958e67b7caea8b4cd444
Dejan85 <xypnotica@gmail.com>
2021-04-30 22:58:25 +0200

    added style to the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -44,24 +44,3 @@ const NavBar = () => {
 };
 
 export default NavBar;
-
-{
-  /* <FiAlignJustify />
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link to="/tags">Tags</Link>
-        </li>
-        <li>
-          <Link to="/recipes">Recipes</Link>
-        </li>
-      </ul> */
-}
