1e0d0200db329b61d1ba531c48860dd19f21a5a9
Dejan85 <xypnotica@gmail.com>
2021-04-30 22:58:12 +0200

    added style to the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,52 @@
 import React from "react";
 import { Link } from "gatsby";
+import { FiAlignJustify } from "react-icons/fi";
+import logo from "../assets/images/logo.svg";
 
 const NavBar = () => {
   return (
-    <nav>
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="img" />
+          </Link>
+          <button className="nav-btn">
+            <FiAlignJustify />
+          </button>
+        </div>
+        <div className="nav-links show-links">
+          <Link to="/" className="nav-link" activeClassName="active-link">
+            Home
+          </Link>
+          <Link
+            to="/recipes"
+            className="nav-link"
+            activeClassName="active-link"
+          >
+            Recipes
+          </Link>
+          <Link to="/tags" className="nav-link" activeClassName="active-link">
+            Tags
+          </Link>
+          <Link to="/about" className="nav-link" activeClassName="active-link">
+            About
+          </Link>
+          <div className="nav-link contact-link">
+            <Link to="/contact" className="btn">
+              Contact
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
+
+{
+  /* <FiAlignJustify />
       <ul>
         <li>
           <Link to="/">Home</Link>
@@ -20,9 +63,5 @@ const NavBar = () => {
         <li>
           <Link to="/recipes">Recipes</Link>
         </li>
-      </ul>
-    </nav>
-  );
-};
-
-export default NavBar;
+      </ul> */
+}
