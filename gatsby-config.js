04c7b2b68bc705ee07a591568e6aa9e27b2bde91
Dejan85 <xypnotica@gmail.com>
2021-05-01 14:50:15 +0200

    added meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,17 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Simple Recipes",
+    description: "Nice and clean recipes site",
+    author: "@dejanmarkovic",
+    person: { name: "Dejan", age: 35 },
+    simpleData: ["item 1", "item 2"],
+    complexData: [
+      { name: "Dejan", age: 35 },
+      { name: "Sara", age: 6 },
+    ],
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
