148cf36c7dd2d02ed85e253e243fa98dc5730796
Dejan85 <xypnotica@gmail.com>
2021-05-03 01:34:41 +0200

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,3 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
   /* Your site config here */
   siteMetadata: {
@@ -22,5 +16,12 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
   ],
 };
