7c64ce711132ea0ad505fb5c1f87681b276b4c71
Dejan85 <xypnotica@gmail.com>
2021-05-03 16:09:07 +0200

    created gatsby-node-js

---
+++ gatsby-node.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+exports.createPage = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  const result = await graphql(`
+    query GetRecipes {
+      allContentfulRecipe {
+        nodes {
+          content {
+            tags
+          }
+        }
+      }
+    }
+  `);
+
+  result.data.allContentfulRecipe.nodes.forEach(recipe => {
+    recipe.content.tags.forEach(tag => {
+      createPage({
+        path: `/${tag}`,
+        component: path.resolve(`src/teplates/tag-template.js`),
+        context: {
+          tag: tag,
+        },
+      });
+    });
+  });
+};
